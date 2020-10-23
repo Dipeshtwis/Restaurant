db71ea534fb5cb9d953990025402801ea60c89ed
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-10-23 17:39:29 +0530

    Add home module

---
+++ src/home.js
@@ -0,0 +1,17 @@
+const home = () => {
+  const spec = document.createElement('div');
+  
+  const head = document.createElement('h1');
+  head.innerHTML = 'Welcome to the Fast Food Restaurant';
+  head.style.cssText = 'color :white; font-size: 48px; text-align: center; margin-top: 3rem;';
+  spec.appendChild(head);
+
+  const para = document.createElement('p');
+  para.innerHTML = 'Indian Accent is one of New Delhi’s, in fact India’s most prized restaurants. Chef Manish Mehrotra expertly crafts Indian dishes so that they retain their original charm but also have a touch of modernity. Known for its butter chicken kulchas, melt-in-your-mouth naans, delicious dals and exquisitely plated, inventive desserts, Indian Accent is the place to be for a dinner to remember.';
+  para.style.cssText = 'color :white; font-size: 24px; line-height: 40px; padding: 5rem;';
+  spec.appendChild(para);
+
+  return spec;
+};
+
+export default home;
