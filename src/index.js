eb546f59a310709e82b9743f0d196028b08028d1
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-10-23 21:25:38 +0530

    Add eventlistener to navbar-items

--- src/index.js
+++ src/index.js
@@ -4,9 +4,29 @@ import menu from './menu';
 import contact from './contact';
 
 const parent = document.getElementById('content');
-parent.style.background = "url('assets/img/b.jpg') center";
+parent.style.background = "url('assets/img/b.jpg') center no-repeat";
 
 parent.appendChild(nav());
-parent.appendChild(home());
-parent.appendChild(menu());
-parent.appendChild(contact());
+const link = document.querySelectorAll('.links');
+const main = document.createElement('div');
+
+main.appendChild(home());
+parent.appendChild(main);
+
+const tab = (lid) => {
+  let a;
+  if (lid === 'home') a = home();
+  if (lid === 'menu') a = menu();
+  if (lid === 'contact') a = contact();
+
+  return a;
+};
+
+
+link.forEach((li) => {
+  li.addEventListener('click', () => {
+    const element = tab(li.id);
+    main.innerHTML = '';
+    main.appendChild(element);
+  });
+});
