a780b1acc2b3a5d24130593babf7667fd8e63fe2
Dipeshtwis <deepesh.97kumar@gmail.com>
2020-10-23 19:39:32 +0530

    Add recipe images

--- src/menu.js
+++ src/menu.js
@@ -2,23 +2,25 @@ const menu = () => {
   const spec = document.createElement('div');
   spec.style.cssText = 'display: grid; grid-template-columns: repeat(2,1fr); padding:3rem;';
 
+  // menu1
+
   const menu1 = document.createElement('div');
   spec.appendChild(menu1);
 
   const head = document.createElement('h3');
   head.innerHTML = 'Noodles';
-  head.style.cssText = 'color :white; font-size: 24px; margin: .5rem 0;';
+  head.style.cssText = 'color :white; font-size: 24px; margin: 1rem 0;';
   menu1.appendChild(head);
 
   const img = document.createElement('img');
   img.style.cssText = 'height: 200px';
-  img.src = 'b.jpg';
+  img.src = 'noodle.jpg';
   img.alt = 'img';
   menu1.appendChild(img);
 
   const para = document.createElement('p');
   para.innerHTML = 'Rupee 100 per/plate';
-  para.style.cssText = 'color :white; font-size: 12px; line-height: 30px; padding: 3rem;';
+  para.style.cssText = 'color :white; font-size: 18px; line-height: 30px; padding: .5rem;';
   menu1.appendChild(para);
 
   // menu2
@@ -28,18 +30,18 @@ const menu = () => {
 
   const title = document.createElement('h3');
   title.innerHTML = 'Pasta';
-  title.style.cssText = 'color :white; font-size: 24px; margin: .5rem 0;';
+  title.style.cssText = 'color :white; font-size: 24px; margin: 1rem 0;';
   menu2.appendChild(title);
 
   const imge = document.createElement('img');
   imge.style.cssText = 'height: 200px';
-  imge.src = 'b.jpg';
+  imge.src = 'pastaa.jpg';
   imge.alt = 'imge';
   menu2.appendChild(imge);
 
   const desc = document.createElement('p');
   desc.innerHTML = 'Rupee 80 per/plate';
-  desc.style.cssText = 'color :white; font-size: 12px; line-height: 30px; padding: 3rem;';
+  desc.style.cssText = 'color :white; font-size: 18px; line-height: 30px; padding: 1rem;';
   menu2.appendChild(desc);
 
   // menu3
@@ -48,19 +50,19 @@ const menu = () => {
   spec.appendChild(menu3);
 
   const ti = document.createElement('h3');
-  ti.innerHTML = 'Coffee';
-  ti.style.cssText = 'color :white; font-size: 24px; margin: .5rem 0;';
+  ti.innerHTML = 'Biryani';
+  ti.style.cssText = 'color :white; font-size: 24px; margin: 1rem 0;';
   menu3.appendChild(ti);
 
   const image = document.createElement('img');
   image.style.cssText = 'height: 200px';
-  image.src = 'b.jpg';
+  image.src = 'biryani.jpg';
   image.alt = 'imge';
   menu3.appendChild(image);
 
   const des = document.createElement('p');
-  des.innerHTML = 'Rupee 150 cup';
-  des.style.cssText = 'color :white; font-size: 12px; line-height: 30px; padding: 3rem;';
+  des.innerHTML = 'Rupee 150 per/plate';
+  des.style.cssText = 'color :white; font-size: 18px; line-height: 30px; padding: .5rem;';
   menu3.appendChild(des);
 
   // menu4
@@ -69,19 +71,19 @@ const menu = () => {
   spec.appendChild(menu4);
 
   const header = document.createElement('h3');
-  header.innerHTML = 'Chowmein';
-  header.style.cssText = 'color :white; font-size: 24px; margin: .5rem 0;';
+  header.innerHTML = 'Papdi Chaat';
+  header.style.cssText = 'color :white; font-size: 24px; margin: 1rem 0;';
   menu4.appendChild(header);
 
   const im = document.createElement('img');
   im.style.cssText = 'height: 200px';
-  im.src = 'b.jpg';
+  im.src = 'chaat.jpg';
   im.alt = 'imge';
   menu4.appendChild(im);
 
   const de = document.createElement('p');
   de.innerHTML = 'Rupee 150 cup';
-  de.style.cssText = 'color :white; font-size: 12px; line-height: 30px; padding: 3rem;';
+  de.style.cssText = 'color :white; font-size: 18px; line-height: 30px; padding: .5rem;';
   menu4.appendChild(de);
 
   return spec;
